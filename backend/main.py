660fca37d4b3d9e2b88b28dd00392bd2c53f4826
Ahram Kim <ahram0223@gmail.com>
2026-06-02 16:55:28 +0900

    chore: main.py에 CORS 미들웨어 설정 추가

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from router import region 
 
 app = FastAPI()
+
+# CORS 설정 추가
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # 라우터 등록
 app.include_router(region.router)
